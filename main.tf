9eb00627a961bcdfda6feeafc315dd4bd6e07005
santoshrawlo <143497046+rawlo123@users.noreply.github.com>
2025-12-12 17:13:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0ecb62995f68bb549"
   instance_type          = "c7i-flex.large"
-  key_name               = "default"
+  key_name               = "santukey"
   vpc_security_group_ids = ["sg-0168a08e83b7ecc77"]
   tags = {
     Name = var.instance_names[count.index]
@@ -16,4 +16,5 @@ resource "aws_instance" "one" {
 variable "instance_names" {
   default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
+
 
